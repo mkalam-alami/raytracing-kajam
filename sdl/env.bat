da3ae69bca5a5741a8e7202f907786d11cdc8491
Marwane Kalam-Alami <marwane@kalam-alami.net>
2020-12-24 11:01:38 +0100

    unfinished sdl rust test

---
+++ sdl/env.bat
@@ -0,0 +1,3 @@
+SET PATH=%PATH%;C:\Users\my_user\dev\vcpkg\installed\x64-windows\bin
+SET INCLUDE=%INCLUDE%;C:\Users\my_user\dev\vcpkg\installed\x64-windows\include
+SET LIB=%LIB%;C:\Users\my_user\dev\vcpkg\installed\x64-windows\lib
